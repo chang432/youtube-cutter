2e79a29fe430c8c4cc809fdb7bdbcc51e1f40e19
chang432 <andre888chang@gmail.com>
2023-05-15 18:13:09 -0400

    get youtube url downloading tested

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect
 from flask_s3 import FlaskS3
 from flask_restful import Api
 from api.HelloApiHandler import HelloApiHandler
+import yt_dlp
 
 app = Flask(__name__, template_folder='frontend/dist', static_folder='frontend/dist/assets')
 # CORS(app)
@@ -13,6 +14,24 @@ api = Api(app)
 def say_hello(name):
     print(f"hellooo {name}")
     return f'Hello, {name}!'
+
+@app.route('/download/<vid>')
+def youtube_download(vid):
+    url = "https://www.youtube.com/watch?v=" + vid
+    ydl_opts = {
+        'format': 'mp3/bestaudio/best',
+        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }],
+        'postprocessor_args': ["-ss", "00:00:30", "-to", "00:01:00"]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download([url])
+
+    return f'Hello, {vid}!'
 
 api.add_resource(HelloApiHandler, '/flask/hello')
 
--- dlmp3.py
+++ dlmp3.py
@@ -1,21 +1,17 @@
-from __future__ import unicode_literals
-import youtube_dl
+import yt_dlp
 
-# set the video URL and output file name
-video_url = ""
+URLS = ['https://www.youtube.com/watch?v=F35291LbOMM']
 
-# set options for youtube_dl
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
+    'format': 'mp3/bestaudio/best',
+    # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+    'postprocessors': [{  # Extract audio using ffmpeg
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
-        'preferredquality': '192',
     }],
     'postprocessor_args': ["-ss", "00:00:30", "-to", "00:01:00"]
 }
 
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    error_code = ydl.download(URLS)
 
-# download the audio using youtube_dl
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([video_url])
