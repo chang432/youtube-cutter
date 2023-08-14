5ea0c3ab05619eccffca4483149389073e8e499d
chang432 <andre888chang@gmail.com>
2023-08-13 23:55:30 -0400

    download file is youtube title

--- api/CutDownloadHandler.py
+++ api/CutDownloadHandler.py
@@ -3,7 +3,19 @@ from flask import Flask, render_template, send_file, request
 from pytube import YouTube
 import boto3
 import os
+import re
 import subprocess
+
+def sanitize(title: str):
+    title = re.sub(r'[^\x00-\x7f]',r'', title)
+    title = title.lower()
+    # title = title.replace('free', '')
+    title = title.strip()
+    title = title.replace(' ','_')
+    toReplace = '[](){}"“.,@#*&<>:;/\\|' + "'"
+    for char in toReplace:
+        title = title.replace(char, '')
+    return title
 
 class CutDownloadHandler(Resource):
   def post(self):
@@ -18,6 +30,11 @@ class CutDownloadHandler(Resource):
 
     s3_client = boto3.client('s3')
     bucket_name = "youtube-cutter-static-files-dev"
+
+    # getting video title
+    url = "https://www.youtube.com/watch?v=" + yt_id
+    yt = YouTube(url, use_oauth=False, allow_oauth_cache=False)
+    yt_title = sanitize(yt.title)
 
     # download full file from s3 (previously uploaded)
     full_file = yt_id + ".mp3"
@@ -54,7 +71,7 @@ class CutDownloadHandler(Resource):
     # uploading cut file to s3
     s3 = boto3.resource('s3')
     bucket_name = 'youtube-cutter-static-files-dev'
-    file_key = f"audio/{yt_id}-cut.mp3" if audio_type == "MP3" else f"audio/{yt_id}-cut.wav"
+    file_key = f"audio/{yt_title}.mp3" if audio_type == "MP3" else f"audio/{yt_title}.wav"
     s3.meta.client.upload_file(cut_file, bucket_name, file_key, ExtraArgs={'ACL': 'public-read'})
 
     print(f"[CUSTOM] upload of cut file {bucket_name}/{file_key} complete! Now sending s3 url as response...")
