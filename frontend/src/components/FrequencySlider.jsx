591613bff3fca0673be0c9275627484316e644b2
chang432 <andre888chang@gmail.com>
2025-01-22 18:05:40 -0500

    Add logic to be able to edit frequency cutoff range values directly not just through the slider

--- frontend/src/components/FrequencySlider.jsx
+++ frontend/src/components/FrequencySlider.jsx
@@ -1,22 +1,153 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Slider } from "antd" ;
 
 const FrequencySlider = ({freqRange, setFreqRange, freqLimit}) => {
-    function onSliderChange(value) {
-        if (value[0] >= value[1]) {
-            return;
-        }
+    const [freqRangeText, setFreqRangeText] = useState([freqRange[0], freqRange[1]]);
+    const freqRangeTextRef = useRef([freqRangeText[0], freqRangeText[1]]);
+    let clickedInsideLowCutInputBox = { value: false };
+    let clickedInsideHighCutInputBox = { value: false };
 
-        setFreqRange([value[0],value[1]])
+    function handleLowCutInputChange(event) {
+        setFreqRangeText([parseInt(event.target.value), freqRangeText[1]]);
+        console.log("Input changed to " + freqRangeText);
     }
+
+    function handleHighCutInputChange(event) {
+        setFreqRangeText([freqRangeText[0], parseInt(event.target.value)]);
+    }
+
+    function keydownEnterEventHandler(event, isLowCut) {
+        if (event.key === "Enter") {
+            console.log(
+                "enter pressed"
+            );
+            if (isLowCut) {
+                setFreqRange([freqRangeTextRef.current[0], freqRange[1]]);
+            } else {
+                setFreqRange([freqRange[0], freqRangeTextRef.current[1]]);
+            }
+        }
+    }
+
+    function outsideInputBoxClickEventHandler(
+        event,
+        inputElement,
+        clickedInsideInputBox,
+        isLowCut
+    ) {
+        if (
+            !inputElement.contains(event.target) &&
+            clickedInsideInputBox.value
+        ) {
+            // Execute your code here
+            console.log(
+                "outside input box pressed"
+            );
+
+            if (isLowCut) {
+                console.log("Setting freq range to " + freqRangeTextRef.current[0] + " " + freqRange[1]);
+                setFreqRange([freqRangeTextRef.current[0], freqRange[1]]);
+            } else {
+                setFreqRange([freqRange[0], freqRangeTextRef.current[1]]);
+            }
+
+            clickedInsideInputBox.value = false;
+        }
+    }
+
+    useEffect(() => {
+        freqRangeTextRef.current = [freqRangeText[0], freqRangeText[1]];
+    }, [freqRangeText]);
+
+    useEffect(() => {
+        let lowcutInputElement = document.getElementById("lowCutInput");
+
+        const keydownEnterEventHandlerRef = (event) =>
+            keydownEnterEventHandler(event, true);
+        lowcutInputElement.addEventListener("keydown", keydownEnterEventHandlerRef);
+
+        const insideInputBoxClickEventHandlerRef = () => {
+            console.log("inside input box clicked");
+            clickedInsideLowCutInputBox.value = true;
+        };
+        lowcutInputElement.addEventListener(
+            "click",
+            insideInputBoxClickEventHandlerRef
+        );
+
+        const outsideInputBoxClickEventHandlerRef = (event) =>
+            outsideInputBoxClickEventHandler(
+                event,
+                lowcutInputElement,
+                clickedInsideLowCutInputBox,
+                true
+            );
+        document.addEventListener("click", outsideInputBoxClickEventHandlerRef);
+
+        return () => {
+            // cleanup on unmount
+            lowcutInputElement.removeEventListener(
+                "keydown",
+                keydownEnterEventHandlerRef
+            );
+            lowcutInputElement.removeEventListener(
+                "click",
+                insideInputBoxClickEventHandlerRef
+            );
+            document.removeEventListener(
+                "click",
+                outsideInputBoxClickEventHandlerRef
+            );
+        };
+    }, []);
+
+    useEffect(() => {
+        let highcutInputElement = document.getElementById("highCutInput");
+
+        const keydownEnterEventHandlerRef = (event) =>
+            keydownEnterEventHandler(event, false);
+        highcutInputElement.addEventListener("keydown", keydownEnterEventHandlerRef);
+
+        const insideInputBoxClickEventHandlerRef = () => {
+            console.log("inside input box clicked");
+            clickedInsideHighCutInputBox.value = true;
+        };
+        highcutInputElement.addEventListener(
+            "click",
+            insideInputBoxClickEventHandlerRef
+        );
+
+        const outsideInputBoxClickEventHandlerRef = (event) =>
+            outsideInputBoxClickEventHandler(
+                event,
+                highcutInputElement,
+                clickedInsideHighCutInputBox,
+                false
+            );
+        document.addEventListener("click", outsideInputBoxClickEventHandlerRef);
+
+        return () => {
+            // cleanup on unmount
+            highcutInputElement.removeEventListener(
+                "keydown",
+                keydownEnterEventHandlerRef
+            );
+            highcutInputElement.removeEventListener(
+                "click",
+                insideInputBoxClickEventHandlerRef
+            );
+            document.removeEventListener(
+                "click",
+                outsideInputBoxClickEventHandlerRef
+            );
+        };
+    }, []);
 
     return (
         <div className="flex flex-col items-center w-56">
             <div className="flex flex-row justify-between w-full items-center">
-                {/* <p>{Math.round(freqFloor / 100 * freqLimit)}Hz</p>
-                <p>{Math.round(freqCeiling / 100 * freqLimit)}Hz</p> */}
-                <p>{freqRange[0]}Hz</p>
-                <p>{freqRange[1]}Hz</p>
+                <input type="text" id="lowCutInput" defaultValue={0} value={freqRangeText[0]} onChange={handleLowCutInputChange}/>
+                <input type="text" id="highCutInput" defaultValue={freqLimit} value={freqRangeText[1]} onChange={handleHighCutInputChange}/>
             </div>
             <Slider
                 className="flex flex-col items-center w-full" 
@@ -25,7 +156,7 @@ const FrequencySlider = ({freqRange, setFreqRange, freqLimit}) => {
                 max={freqLimit}
                 value={freqRange}
                 tooltip={{ open: false }} // Disables the tooltip
-                onChange={onSliderChange}
+                onChange={(value) => {setFreqRange([value[0],value[1]]);setFreqRangeText([value[0],value[1]])}}
             >
             </Slider>
         </div>
