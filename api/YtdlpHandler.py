2254e1a9f19b1d0c122e356dbd215bf4b4da2b1b
chang432 <andre888chang@gmail.com>
2025-02-04 21:13:56 -0500

    Change cookie + po_token location from code to s3 + ssm

--- api/YtdlpHandler.py
+++ api/YtdlpHandler.py
@@ -1,39 +1,45 @@
 import yt_dlp
 import shutil
+import boto3
 import os
 
 class YtdlpHandler:
     destination = None
-    po_token = "MluRGnGq1eOGf5EiVvoO5btNAErf5PyD8dLY9nTxoPZo9Idy-Bv834_datyqAiGXnFoOiMTXL1pgL1f0hl2m1JXAHAr1v5XG_MNwAFDhBG-hLo4BvAq2aoKeyt8d"  # CHANGEME
 
     def __init__(self, url) -> None:
-        self.url = url
+        s3_client = boto3.client("s3")
+        ssm_client = boto3.client("ssm")
 
-    def yt_dlp_monitor(d):
+        self.url = url
+        self.po_token = ssm_client.get_parameter(Name="youtube-cutter-dev-po-token")
+        
+        # Need to move cookies to tmp folder because that is the only area that is non read only for lambdas
+        s3_client.download_file("youtube-cutter-private-dev", "youtube-credentials/cookies.txt", "/tmp/cookies.txt")
+
+    def yt_dlp_monitor(self, d):
+        print(d)
         YtdlpHandler.destination  = d.get('info_dict').get('_filename')
         print("FILE NAME IS: ", YtdlpHandler.destination)
 
-    yt_dlp_opts = {
-        'format': 'm4a/bestaudio/best', 
-        'quiet': True, 
-        'paths': {'home': '/tmp/'}, 
-        'progress_hooks': [yt_dlp_monitor],
-        # 'username': 'oauth2',
-        # 'password': '',
-        # 'extractor_args': {'youtube': 'player-client=web;po_token=web+'+po_token},
-        'extractor_args': {'youtube': 'player_client=web_creator;po_token=web_creator+'+po_token},
-        'cookiefile': '/tmp/cookies.txt',
-        'verbose': True,
-        'cachedir': '/tmp',
-        'nocachedir': True,
-        'ignoreerrors': True
-    }
-
     def yt_dlp_request(self, shouldDownload=False):
-        # Need to move cookies to tmp folder because that is the only area that is non read only for lambdas
-        shutil.copyfile("./cookies/cookies.txt","/tmp/cookies.txt")
 
-        with yt_dlp.YoutubeDL(self.yt_dlp_opts) as ydl:
+        yt_dlp_opts = {
+            'format': 'm4a/bestaudio/best', 
+            'quiet': True, 
+            'paths': {'home': '/tmp/'}, 
+            'progress_hooks': [self.yt_dlp_monitor],
+            # 'username': 'oauth2',
+            # 'password': '',
+            # 'extractor_args': {'youtube': 'player-client=web;po_token=web+'+po_token},
+            'extractor_args': {'youtube': 'player_client=web_creator;po_token=web_creator+'+self.po_token["Parameter"]["Value"]},
+            'cookiefile': '/tmp/cookies.txt',
+            'verbose': True,
+            'cachedir': '/tmp',
+            'nocachedir': True,
+            'ignoreerrors': True
+        }
+
+        with yt_dlp.YoutubeDL(yt_dlp_opts) as ydl:
             output = ydl.extract_info(self.url, download=shouldDownload)
         title = output.get('title', None)
         duration = output.get('duration', None)  # Duration in seconds
