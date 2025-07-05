f6c50e0a77a0dd62d7564fc813c60643f909bf2a
chang432 <andre888chang@gmail.com>
2025-07-05 09:28:23 -0400

    Add ssh authentication to upload hetzner helper script

--- docker/upload_hetzner_vps.sh
+++ docker/upload_hetzner_vps.sh
@@ -1,5 +1,5 @@
 VPS_IP="178.156.164.80"
 
-scp -r ~/.aws root@${VPS_IP}:/root/.aws
+scp -r -i ~/.ssh/id_ed25519 ~/.aws root@${VPS_IP}:/root/.aws
 
-scp -r ../docker root@${VPS_IP}:/opt/
+scp -r -i ~/.ssh/id_ed25519 ../docker root@${VPS_IP}:/opt/
