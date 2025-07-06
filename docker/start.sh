6c92633b1ab5aa5b4d7a335d9f0d65ff61241c76
chang432 <andre888chang@gmail.com>
2025-07-05 22:00:11 -0400

    Update start cron logic + add cloud init code

--- docker/start.sh
+++ docker/start.sh
@@ -2,8 +2,12 @@
 
 # Hetzner vps startup cron script
 
-if [[ -d "/mnt/HC_Volume_102861833" && $(docker ps -q | wc -l) == 0 ]]; then
+PARTITION_NAME="HC_Volume_102861833"
+
+if [[ -d "/mnt/${PARTITION_NAME}" && $(docker ps -q | wc -l) == 0 ]]; then
         echo "external partition detected and containers are not running yet, starting up now!"
+        
+        cp -r "/mnt/${PARTITION_NAME}/.aws" ~/.aws
         cd /opt/docker
         docker-compose build
         docker-compose up -d
