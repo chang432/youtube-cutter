74f8f7f0a97b41c09b6b8e90755f049b2c1fd8b3
chang432 <andre888chang@gmail.com>
2025-07-06 10:24:41 -0400

    Update cron

--- docker/start.sh
+++ docker/start.sh
@@ -4,11 +4,20 @@
 
 PARTITION_NAME="HC_Volume_102861833"
 
+if [[ ! -d "/mnt/${PARTITION_NAME}" ]]; then
+    echo "External volume not detected, attempting to mount"
+    mkdir -p "/mnt/${PARTITION_NAME}"
+    mount -o discard,defaults "/dev/disk/by-id/scsi-0${PARTITION_NAME}" "/mnt/${PARTITION_NAME}"
+fi
+
 if [[ -d "/mnt/${PARTITION_NAME}" && $(docker ps -q | wc -l) == 0 ]]; then
-        echo "external partition detected and containers are not running yet, starting up now!"
-        
-        cp -r "/mnt/${PARTITION_NAME}/.aws" ~/.aws
-        cd /opt/docker
-        docker-compose build
-        docker-compose up -d
+    echo "External volume detected and containers are not running yet, starting up now!"
+    
+    cp -r "/mnt/${PARTITION_NAME}/.aws" ~/.aws
+    cd /opt/docker
+    docker-compose build
+    docker-compose up -d
+
+    echo "containers started up successfully, turning off cron..."
+    crontab -l 2>/dev/null | sed '/start.sh/ s/^/#/' | crontab -
 fi
