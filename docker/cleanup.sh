066a87bea203d82b5e73f2722cc372f9fe2dad39
chang432 <andre888chang@gmail.com>
2025-07-06 14:01:46 -0400

    Change cleanup to use access instead of modified date

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -13,7 +13,7 @@ echo "Current epoch time: ${CURRENT_EPOCH_TIME}"
 
 for file in ${AUDIO_PATH}/*; do
     if [[ -f "$file" ]]; then
-        file_epoch=$(stat -c %Y $file)
+        file_epoch=$(stat -c %X $file)
         echo "${file} -> ${file_epoch}"
         epoch_diff=$(( $CURRENT_EPOCH_TIME - $file_epoch ))
         if [[ $epoch_diff -gt ${TTL_SEC} ]]; then
