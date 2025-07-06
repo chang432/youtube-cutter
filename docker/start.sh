f87674f3ce8e2675229634c09e23e9ad94b60009
chang432 <andre888chang@gmail.com>
2025-07-05 21:22:48 -0400

    Fix start cron script

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,7 @@
 # Hetzner vps startup cron script
 
 if [[ -d "/mnt/HC_Volume_102861833" && $(docker ps -q | wc -l) == 0 ]]; then
-        print("external partition detected and containers are not running yet, starting up now!"
+        echo "external partition detected and containers are not running yet, starting up now!"
         cd /opt/docker
         docker-compose build
         docker-compose up -d
