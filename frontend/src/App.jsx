7bea754ca9ab8d980f495b7ae36a5cd0589dbfe5
chang432 <andre888chang@gmail.com>
2025-07-11 21:52:37 -0400

    move migration notice text top the top center of page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -635,6 +635,7 @@ function App() {
             >
                 <FontAwesomeIcon icon={faCircleUp} className="fa-2xl" />
             </button>
+            <h1 className="fixed top-4 left-1/2 transform -translate-x-1/2 text text-blue-600">As of 07/11/2025, we have migrated servers! Please contact us if there are any issues.</h1>
             <ThemeSwitch
                 isDarkMode={isDarkMode}
                 setIsDarkMode={setIsDarkMode}
@@ -642,7 +643,6 @@ function App() {
             />
             <div className={`flex flex-col justify-center ${displayCutterUI ? "h-fit py-20" : "h-screen"} items-center`}>
                 {/* <h1 className="text text-red-600">We are aware of some issues, please hold while we fix</h1> */}
-                <h1 className="text text-blue-600">As of 07/11/2025, we have migrated servers! Please contact us if there are any issues.</h1>
                 <div className="flex flex-col justify-center items-center w-full" >
                     <button onClick={goHome} className="text-8xl mb-4">
                         wav.ninja
