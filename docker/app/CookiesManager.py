8c76ac0a50e35e71e2cc3b2adbb4e209f55bfdd3
chang432 <andre888chang@gmail.com>
2025-10-11 15:21:19 -0400

    Refactor resync cookie logic

--- docker/app/CookiesManager.py
+++ docker/app/CookiesManager.py
@@ -29,7 +29,10 @@ class CookiesManager:
                 raise ValueError("No cookies found in S3 bucket under the specified prefix.")
 
             cookies_status = {}
-            cookies_status["current_cookie"] = None
+            cookies_status["current_cookie"] = {
+                "path": None,
+                "index": -1
+            }
 
             cookies_info = []
             for obj in response['Contents']:
@@ -68,22 +71,17 @@ class CookiesManager:
         with open(cookies_status_path, "r") as json_file:
             cookies_status = json.load(json_file)
 
-        current_cookie = cookies_status.get("current_cookie", None)
+        current_cookie = cookies_status["current_cookie"].get("path", None)
 
         if not current_cookie:
             raise ValueError("No valid cookies found in cookies_status.json")
 
         return current_cookie
 
-    def retrieve_cookie_path(self, cookie_name):
-        cookie_path = self.cookies_staging + "/" + cookie_name
-        if not os.path.exists(cookie_path):
-            raise FileNotFoundError(f"Cookie named {cookie_name} does not exist. Please initialize first.")
-        return cookie_path
     
     def resync_cookies(self):
         """
-        Pulls down any cookies that have been changed in s3
+        Sync cookies from S3 and update state of each cookie. 
         """
         json_file_path = os.path.join(self.cookies_staging, "cookies_status.json")
 
@@ -93,27 +91,40 @@ class CookiesManager:
         with open(json_file_path, "r") as json_file:
             cookies_status = json.load(json_file)
 
-        for cookie in cookies_status:
-            file_name = cookie["name"]
-            local_file_path = os.path.join(self.cookies_staging, file_name)
-            s3_key = f"{COOKIES_PREFIX}/{file_name}"
+        local_cookie_names = [cookie["name"] for cookie in cookies_status["cookies"]]
 
-            try:
-                # Get S3 file's last modified date
-                s3_metadata = self.s3_client.head_object(Bucket=BUCKET_NAME, Key=s3_key)
-                s3_last_modified = s3_metadata["LastModified"]
+        remote_response = self.s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=COOKIES_PREFIX)
+
+        for obj in remote_response.get('Contents', []):
+            file_name = obj['Key'].split('/')[-1]
+            local_file_path = os.path.join(self.cookies_staging, file_name)
+
+            if not file_name:
+                continue
+
+            if file_name in local_cookie_names: 
+                cur_cookie = cookies_status["cookies"][local_cookie_names.index(file_name)]
+                remote_last_modified = obj['LastModified']
 
                 # Compare dates
-                local_last_modified = datetime.fromisoformat(cookie["lastModified"])
-                Logger.log(f"Comparing S3 last modified ({s3_last_modified}) with local last modified ({local_last_modified}) for {file_name}")
-                if s3_last_modified > local_last_modified:
+                local_last_modified = datetime.fromisoformat(cur_cookie["lastModified"])
+                Logger.log(f"Comparing S3 last modified ({remote_last_modified}) with local last modified ({local_last_modified}) for {file_name}")
+                if remote_last_modified > local_last_modified:
                     # Download updated file
-                    Logger.log(f"Pulling updated cookie {file_name} from s3 because (s3: {s3_last_modified} > local: {local_last_modified})")
-                    self.s3_client.download_file(BUCKET_NAME, s3_key, local_file_path)
-                    cookie["lastModified"] = str(s3_last_modified)
-                    cookie["valid"] = True
-            except Exception as e:
-                print(f"Error checking refresh status or refreshing {file_name}: {e}")
+                    Logger.log(f"Pulling updated cookie {file_name} from s3 because (s3: {remote_last_modified} > local: {local_last_modified})")
+                    self.s3_client.download_file(BUCKET_NAME, obj['Key'], local_file_path)
+                    cur_cookie["lastModified"] = str(remote_last_modified)
+                    cur_cookie["valid"] = False
+            else:
+                Logger.log(f"Pulling new cookie {file_name} from s3")
+                self.s3_client.download_file(BUCKET_NAME, obj['Key'], local_file_path)
+
+                # Add new cookie to cookies_status
+                cookies_status["cookies"].append({
+                    "name": file_name,
+                    "valid": False,
+                    "lastModified": str(datetime.now(timezone.utc))
+                })
 
         # Update cookies_status.json
         with open(json_file_path, "w") as json_file:
