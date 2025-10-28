c92c94eee54d2aa1830668d8af8d0a0b1f3a1cf2
chang432 <andre888chang@gmail.com>
2025-10-28 19:12:14 -0400

    Remove premium related logic on flask side

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_s3 import FlaskS3
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from api.HelloApiHandler import HelloApiHandler
-from api.CleanupHandler import CleanupHandler
-from api.KofiWebhookHandler import KofiWebhookHandler
-from api.PremiumLoginHandler import PremiumLoginHandler
-from api.PremiumVerificationHandler import PremiumVerificationHandler
 import os
 import boto3
-import os
 
 # env variable for local or deployment build
 os.environ["IS_DEPLOYMENT"] = "FALSE"
@@ -19,32 +13,12 @@ if os.environ["IS_DEPLOYMENT"] == "FALSE":
 
 app = Flask(__name__, template_folder='frontend/dist', static_folder='frontend/dist/assets')
 
-ssm_client = boto3.client("ssm")
-
-param_output = ssm_client.get_parameter(
-    Name="youtube-cutter-premium-jwt-key",
-    WithDecryption=True
-)
-
-jwt_secret_key = param_output["Parameter"]["Value"]
-
-# Configure JWT Secret Key
-app.config["JWT_SECRET_KEY"] = jwt_secret_key
-
-jwt = JWTManager(app)
-
 if os.environ["IS_DEPLOYMENT"] == "FALSE": 
     CORS(app, supports_credentials=True)
 
 app.config['FLASKS3_BUCKET_NAME'] = 'youtube-cutter-static-files-dev'
 s3 = FlaskS3(app)
 api = Api(app)
-
-# api.add_resource(HelloApiHandler, '/flask/hello')
-# api.add_resource(CleanupHandler, '/cleanup')
-api.add_resource(KofiWebhookHandler, '/kofi_webhook')
-api.add_resource(PremiumVerificationHandler, '/verify_premium')
-api.add_resource(PremiumLoginHandler, '/login_premium')
 
 @app.route('/')
 def serve():
